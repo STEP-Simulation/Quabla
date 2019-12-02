4d743932a6b4fe3e0765b4eff5c15040c562d87e
zoooi <zoooi@133.51.97.22>
2019-12-02 18:29:00 +0900

    add : モータモデルの変更

--- src/quabla/parameter/InputParam.java
+++ src/quabla/parameter/InputParam.java
@@ -1,4 +1,4 @@
-package quabla;
+package quabla.parameter;
 
 public class InputParam {
 
@@ -7,21 +7,21 @@ public class InputParam {
 
 	//Length [m]================================================================
 	/**Total Length*/
-	public double l    = 1.888;// 2.406;//[m]
+	public double l    = 3.00; // 1.888;// 2.406;//[m]
 
 	/**Center of gravity @ take-off*/
-	public double lcg0 = 1.054;//1.487;//[m]
+	public double lcg0 = 1.887263;//1.054;//1.487;//[m]
 
 	/**Center of Gravity @ engine-cut-off*/
-	public double lcgf = 1.028;//1.430;//[m]
+	public double lcgf = 1.788345;//1.028;//1.430;//[m]
 
 	/**Center of Gravity of Propellant*/
-	public double lcgp = 0.560;//1.992;  //[m]
+	public double lcgp = 2.245251;// 0.560;//1.992;  //[m]
 
 	/**Outer Diameter*/
-	public double d    = 0.116;//0.120;  //[m]
-	/**Upper Launch Lug from Nosecone*/
+	public double d    = 0.120; //0.116;//0.120;  //[m]
 
+	/**Upper Launch Lug from Nosecone*/
 	public double upper_lug = 0.8;//0.79;//[m]
 
 	/**Lower Launch Lug from Nosecone*/
@@ -30,29 +30,31 @@ public class InputParam {
 
 
 	//Weight [kg]===============================================================
-	public double m0  = 7.243;//6.514;   //weight @lift-off
-	public double mf  = 6.621;//5.708;   //weight @ engnine-cut-off
+	public double m0  = 17.803;// 7.243;//6.514;   //weight @lift-off
+	public double mf  = 13.88359; //6.621;//5.708;   //weight @ engnine-cut-off
 	//==========================================================================
 
 
 
 	//moment of inertria [kg m^2]===============================================
-	public double Ij_pitch_0  = 1.638;//3.47;  //[kg m^2] pitch moment of inertia  @take off
-	public double Ij_pitch_f  = 1.579;//3.288;  //[kg m^2]pitch moment of inertia @engine cut off
-	public double Ij_roll_0  = 0.5 ;   //[kg m^2]roll moment of inertia @lift off
-	public double Ij_roll_f  =0.4 ;   //[kg m^2] roll moment of inertia @ engine cut off
+	public double Ij_pitch_0  = 10.596653;//1.638;//3.47;  //[kg m^2] pitch moment of inertia  @take off
+	public double Ij_pitch_f  = 9.677825; // 1.579;//3.288;  //[kg m^2]pitch moment of inertia @engine cut off
+	public double Ij_roll_0  = 0.040251;//0.5 ;   //[kg m^2]roll moment of inertia @lift off
+	public double Ij_roll_f  = 0.038338;//0.4 ;   //[kg m^2] roll moment of inertia @ engine cut off
+	public double IjStRoll = 0.05 ; //[kg m^2] moment of inertia of structure in roll
+	public double IjStructPitch = 7.0 ; //[kg m^2] moment of inertia of structure in pitch
 	//==========================================================================
 
 
 	//aero parameter============================================================
-	public boolean Cd_file_exist = false;
+	public boolean Cd_file_exist = true;
 	public boolean CNa_file_exist = false;
 	public boolean Lcp_file_exist = false;
 	public String Lcp_file;
-	public String Cd_file;
+	public String Cd_file = "C:\\Users\\zoooi\\Documents\\STEP\\機体班\\シュミレーション\\ForRocket\\ForRocket\\Cd2.csv";
 	public String CNa_file;
 	/**Constant Center of Pressure*/
-	public double Lcp = 1.276;///1.771 ;//[m]
+	public double Lcp = 2.100;//1.276;///1.771 ;//[m]
 
 	/**Constant Coefficient of Drag*/
 	public double Cd  = 0.45;//0.6;    //[-]
@@ -64,41 +66,74 @@ public class InputParam {
 	public double Cmq = -3.00;   //[1/rad]
 
 	/**Coefficient of Aero Dumping Moment in Roll*/
-	public double Clp = -0.10;   //[1/rad]
+	public double Clp = -0.050;   //[1/rad]
 	//==========================================================================
 
 
 
 	//Parachute=============================================
-	public double CdS1 = 1.1690;     //[m/s] falling velocity of 1st parachute
+	/** Product of drag coefficient of parachute(or draug chute) and  */
+	public double CdS1 = 0.209396866;     //[m2]
 	public boolean para2_exist = false;
 											/**
 											 * true : using 2 parachutes
 											 * false : using only a parachute
 											 *  */
-	public double CdS2      = 0.482;   //[m/s] velocity after 2nd para open (falling 1st and 2nd para)
+	public double CdS2      = 0.851268565;   //[m/s] velocity after 2nd para open (falling 1st and 2nd para)
 	public double alt_para2 = 200.0;  //[m]altitude of 2nd para open
 	//==========================================================================
 
 
 	//launcher==================================================================
-	public double elevation_launcher = 70.0;  //[deg] elevation of launcher (vertical = 90.0 deg)
-	public double azimuth_launcher   = 150.0;  //[deg] azimuth of launcher (east = 0 deg , south = 270 deg )
+	public double elevation_launcher = 85.0;  //[deg] elevation of launcher (vertical = 90.0 deg)
+	public double azimuth_launcher   = 290.0;  //[deg] azimuth of launcher (east = 0 deg , south = 270 deg )
 	public double length_Launcher    = 5.0;       //[m]length of launcher
-	public double magnetic_dec       = 7.53;    //[deg] magnetic declination
+	public double magnetic_dec       = 8.9;    //[deg] magnetic declination
 	//==========================================================================
 
 
 
 	//engine configulation======================================================
-	public double dth = 11.45;   //[mm] Nozzle throat diameter
-	public double eps = 2.6951;  //[-] Nozzle expansion raitio
+	/** Nozzle throat diameter */
+	public double dth = 11.45;   //[mm]
+	/** nozzle expansion ratio
+	 * nozzle area ratio */
+	public double eps = 2.6951;  //[-]
+
+	public double massFuelBef;
+
+	public double massFuelAft;
+
+	public double diameterFuelPort;
+
+	public double diameterFuelOut;
+
+	/** oxidizer mass*/
+	public double massOxInit; //[kg]
+
+	/**length of oxidizer's tank*/
+	public double lengthTank ; //[m]
+
+	/** diameter of oxidizer's tank*/
+	public double diameterTank; //[m]
+
+	/** Tank Volume */
+	public double volTank = 4030 ; //[cc]
+
+	/** Oxidizer's Density */
+	public double densityOxidizer ; //[kg/m^3]
+
+	/**length of motor case
+	 * if using HyperTEK series, length of grain*/
+	public double lenghtMotor; //[m]
+
+	public double lengthFuel; //[m]
 	//==========================================================================
 
 
 
 	//wind======================================================================
-	public boolean Wind_file_exsit = false; // true or false
+	public boolean Wind_file_exsit = false;
 	public String wind_file = "C:\\Users\\zoooi\\Documents\\STEP\\機体班\\シュミレーション"
 			+ "\\wind_august_noshiro_nominal.csv";
 	public int WindModel   = 1;  // wind model
@@ -109,12 +144,14 @@ public class InputParam {
 									 * in case of WindModel = 1
 									 * */
 
-	public double wind_azimuth =  -150.0;
-									/** [deg]azimuth of wind
-									 * 0 deg : from west to east
-									 * 270 deg: from north to south
-									 * */
+	/**wind azimuth*/
+	public double wind_azimuth =  -150.0;//[deg]
+	/* 0 deg : from west to east
+	 * 270 deg : from north to south
+	 * **/
+
 	public double wind_speed   = 1.0; 	 //wind speed [m/s];
+	/**wind reference altitude*/
 	public double Zr     = 5.0; 		 //altitude anemometer located [m]
 	public double temperture0  = 15.0; //temperture at 0 m [℃]
 	//==========================================================================
@@ -122,7 +159,9 @@ public class InputParam {
 
 	//wind map configulation============================
 	//落下分散を出すときのみ使用
-	public double speed_min = 1.0; //[m/s]minimun wind speed
+
+	/**minimum wind speed*/
+	public double speed_min = 1.0; //[m/s]
 	public double speed_step = 1.0; //[m/s]風速の刻み幅
 	public int speed_num = 7;   //何風速分計算したいか
 	public int angle_num = 8;
@@ -142,13 +181,13 @@ public class InputParam {
 	 * メインソルバより細かくすると値がおかしくなる
 	 * */
 	public double dt_output = 0.01;//[s]
-	public String  thrustcurve = "C:\\Users\\zoooi\\Documents\\STEP\\機体班\\シュミレーション\\thrust.csv"; //推力データのアドレス
+	public String  thrustcurve = "C:\\Users\\zoooi\\Documents\\STEP\\機体班\\シュミレーション\\thrust_M1000.csv"; //推力データのアドレス
 
 	/**計算結果の出力先のファイルパス
 	 * パスは\\で終わるように
 	 * */
 	public String result_filepath = "C:\\Users\\zoooi\\Documents\\STEP\\機体班\\シュミレーション\\";//出力先のファイルパス
-	public String dir_name = "A";
+	public String dir_name = "test";
 	//==========================================================================
 
 
--- src/quabla/simulator/RocketParameter.java
+++ src/quabla/simulator/RocketParameter.java
@@ -1,8 +1,7 @@
 package quabla.simulator;
 
-import quabla.InputParam;
+import quabla.parameter.InputParam;
 import quabla.simulator.numerical_analysis.Interpolation;
-
 
 /**
  * このクラスはhogehoge
@@ -10,96 +9,136 @@ import quabla.simulator.numerical_analysis.Interpolation;
  * */
 public class RocketParameter {
 
-	public double L , d, S,upper_lug,lower_lug;
-	public double Lcg_0,Lcg_f,Lcg_prop;
-	public double dth , eps , Ath , Ae, de;
-	double m0 , mf , mp0;
-	double Ij_pitch_0, Ij_pitch_f , Ij_roll_0 , Ij_roll_f ;
-	double Ij_dot_pitch, Ij_dot_roll;
-	double Ij_prop_pitch_0 , Ij_prop_roll_0;
-	public double CdS1,CdS2;
-	public boolean para2_exist;
+	public double l, d, S, upperLug, lowerLug;
+	public double lcgBef, lcgAft, lcgPropBef;
+	public double dth, eps, Ath, Ae;
+	/** Engine nozzle diameter [m] */
+	public double de;
+	/** Mass */
+	double m0, mAft, mProp0, massOxBef;
+	private double lengthTank;
+	/** Moment of Inertia */
+	double IjPitchBef, IjPitchAft, IjRollBef, IjRollAft;
+	double IjDotPitch, IjDotRoll;
+	double IjPropPitchBef, IjPropRollBef;
+	private double IjFuelRollBef, IjFuelPitchBef;
+	public double CdS1, CdS2;
+	public boolean para2Exist;
 	public double alt_para2;
+	/** Simulation time step */
 	public double dt;
-	double[][] thrust_data;
-	Interpolation thrust_analy;
-	public double time_Burnout;
-	double launcher_rail;
+	Interpolation thrustAnaly;
+	public double timeBurnout;
+	double lengthLauncherRail;
+	boolean existTipOff;
+	private double IjStPitch, IjStRoll;
+	private double lMotor, dTank, lFuel;
+	private double massFuelBef, massFuelAft;
+	private double diffMassFuel;
+	private double dFuelInBef,dFuelInAft ,dFuelOut;
+	private double lcgFuel;
 
-
-	public RocketParameter(InputParam spec){
+	public RocketParameter(InputParam spec) {
 
 		//// Geometory //////////////////////////////////////////////////
-		this.L = spec.l;
-		this.d = spec.d;
-		this.S = 0.25 * Math.PI * Math.pow(d , 2);
-		this.upper_lug = spec.upper_lug;
-		this.lower_lug = spec.lower_lug;
+		l = spec.l;
+		d = spec.d;
+		S = 0.25 * Math.PI * Math.pow(d, 2);
+		existTipOff = spec.tip_off_exist;
+		if (existTipOff) {
+			upperLug = spec.upper_lug;
+			lowerLug = spec.lower_lug;
+		} else { // launch clearは重心がランチャを抜けたとき
+			upperLug = spec.lcg0;
+			lowerLug = spec.lcg0 + 0.01;
+		}
 
-		this.dth = spec.dth * Math.pow(10, -3);// [mm] => [m]
-		this.eps = spec.eps;	//
-		this.Ath = 0.25 * Math.PI * Math.pow(dth, 2);
-		this.Ae = Ath * eps;
-		this.de = Math.sqrt(Ae * 4 / Math.PI);
+		// Engine configuration
+		dth = spec.dth * Math.pow(10, -3);// [mm] => [m]
+		eps = spec.eps; //
+		Ath = 0.25 * Math.PI * Math.pow(dth, 2);
+		Ae = Ath * eps;
+		de = Math.sqrt(Ae * 4 / Math.PI);
+
+		// fuel(grain)
+		dFuelInBef = spec.diameterFuelPort;
+		dFuelInAft = spec.diameterFuelPort;
+		dFuelOut = spec.diameterFuelOut;
+		lFuel = spec.lengthFuel;
+
+		// oxidizer tank
+		lengthTank = spec.lengthTank;
+		dTank = spec.diameterTank;
 		/////////////////////////////////////////////////////////////////
 
 		///// Center of Gravity /////////////////////////////////////////
-		this.Lcg_0 = spec.lcg0;
-		this.Lcg_f = spec.lcgf;
-		this.Lcg_prop = spec.lcgp;
+		lcgBef = spec.lcg0;
+		lcgAft = spec.lcgf;
+		lcgPropBef = spec.lcgp;
+		lcgFuel = l - (lMotor - 0.5 * lFuel);
+		//TODO l_cgProp の時間変化
+		lMotor = spec.lenghtMotor;
 		/////////////////////////////////////////////////////////////////
 
 		//// Mass ///////////////////////////////////////////////////////
-		this.m0 = spec.m0;
-		this.mf = spec.mf;
-		this.mp0 = m0 - mf;
+		m0 = spec.m0;
+		//massOxBef = spec.massOxInit;
+		massOxBef = (spec.volTank * Math.pow(10, -6)) * spec.densityOxidizer;
+		mAft = spec.mf;
+		mProp0 = m0 - mAft;
+		massFuelBef = spec.massFuelBef;
+		massFuelAft = spec.massFuelAft;
+		diffMassFuel = spec.massFuelBef - massFuelAft;
 		/////////////////////////////////////////////////////////////////
 
 		///// Moment of Inertia /////////////////////////////////////////
-		this.Ij_pitch_0 = spec.Ij_pitch_0;
-		this.Ij_pitch_f = spec.Ij_pitch_f;
-		this.Ij_roll_0 = spec.Ij_roll_0;
-		this.Ij_roll_f = spec.Ij_roll_f;
-		this.Ij_prop_pitch_0 = Ij_pitch_0 - Ij_pitch_f;
-		this.Ij_prop_roll_0 = Ij_roll_0 - Ij_roll_f;
+		IjPitchBef = spec.Ij_pitch_0;
+		IjPitchAft = spec.Ij_pitch_f;
+		IjRollBef = spec.Ij_roll_0;
+		IjRollAft = spec.Ij_roll_f;
+		IjPropPitchBef = IjPitchBef - IjPitchAft;
+		IjPropRollBef = IjRollBef - IjRollAft;
+
+		// Structure's Momtnt of Inertia
+		IjStRoll = spec.IjStRoll;
+		IjStPitch = spec.IjStructPitch;
 		/////////////////////////////////////////////////////////////////
 
 		//// Parachute //////////////////////////////////////////////////
-		this.CdS1 = spec.CdS1;
-		this.para2_exist = spec.para2_exist;
-		if(para2_exist) {
-			this.CdS2 = spec.CdS2;
-			this.alt_para2 = spec.alt_para2;
-		}else {
-			this.CdS2 = 0.0;
-			this.alt_para2 = 0.0;
+		CdS1 = spec.CdS1;
+		para2Exist = spec.para2_exist;
+		if (para2Exist) {
+			CdS2 = spec.CdS2;
+			alt_para2 = spec.alt_para2;
+		} else {
+			CdS2 = 0.0;
+			alt_para2 = 0.0;
 		}
 		/////////////////////////////////////////////////////////////////
 
-		this.dt = spec.dt;
+		dt = spec.dt;
 
 		//// Thrust /////////////////////////////////////////////////////
-		this.thrust_data = GetCsv.get2ColumnArray(spec.thrustcurve);
+		double[][] thrust_data = GetCsv.get2ColumnArray(spec.thrustcurve);
 		double[] time_array = new double[thrust_data.length];
 		double[] thrust_array = new double[thrust_data.length];
-		for(int i = 0; i < thrust_data.length ; i++) {
+		for (int i = 0; i < thrust_data.length; i++) {
 			time_array[i] = thrust_data[i][0];
 			thrust_array[i] = thrust_data[i][1];
 		}
-		this.thrust_analy = new Interpolation(time_array , thrust_array);
-		this.time_Burnout = time_array[thrust_data.length-1];
+		thrustAnaly = new Interpolation(time_array, thrust_array);
+		timeBurnout = time_array[thrust_data.length - 1];
 		/////////////////////////////////////////////////////////////////
 
-		this.launcher_rail = spec.length_Launcher;
+		lengthLauncherRail = spec.length_Launcher;
 	}
-
 
 	public double mdot(double t) {
 		double mdot;
 
-		if(t < time_Burnout) {
-			mdot = (mf - m0) / time_Burnout;// mdot < 0
-		}else {
+		if (t < timeBurnout) {
+			mdot = (mAft - m0) / timeBurnout;// mdot < 0
+		} else {
 			mdot = 0;
 		}
 
@@ -109,12 +148,12 @@ public class RocketParameter {
 	public double[] Ij_dot(double t) {
 		double[] Ij_dot = new double[3];
 
-		if(t < time_Burnout) {
-			Ij_dot[0] = (Ij_roll_f - Ij_roll_0) / time_Burnout;
-			Ij_dot[1] = (Ij_pitch_f - Ij_pitch_0) / time_Burnout;
+		if (t < timeBurnout) {
+			Ij_dot[0] = (IjRollAft - IjRollBef) / timeBurnout;
+			Ij_dot[1] = (IjPitchAft - IjPitchBef) / timeBurnout;
 			Ij_dot[2] = Ij_dot[1];
-		}else {
-			for(int i=0; i<3; i++) {
+		} else {
+			for (int i = 0; i < 3; i++) {
 				Ij_dot[i] = 0.0;
 			}
 		}
@@ -122,65 +161,121 @@ public class RocketParameter {
 		return Ij_dot;
 	}
 
-
-	public double getMass(double t) {
-		double m;
-
-		if(t < time_Burnout) {
-			m = m0 +  (mf - m0) * t / time_Burnout;
-		}else {
-			m = mf;
-		}
-
-		return m;
+	// TODO 修正
+	private double getMassFuel(double t) {
+		return diffMassFuel * t / timeBurnout;
 	}
 
+	/**
+	 * calculate oxidizer's mass
+	 * @param t 時間 [sec]
+	 * @return massOx
+	 * */
+	private double getMassOxidizer(double t)	{
+		if(t < timeBurnout) {
+			return massOxBef * t / timeBurnout;
+		}else {
+			return 0.0;
+		}
+	}
+
+	public double getMass(double t) {
+		if (t < timeBurnout) {
+			return m0 + (mAft - m0) * t / timeBurnout;
+		} else {
+			return mAft;
+		}
+	}
+
+	//TODO 燃焼前後での条件分岐
+	private double getIjPropPitch(double t) {
+		double IjPropPitch;
+		double lcg = getLcg(t);
+
+		// fuel
+		/* 燃料(グレイン)の内径 **/
+		double dFuelIn = dFuelInBef + (dFuelInAft - dFuelInBef) * t / timeBurnout;
+		double massFuel = getMassFuel(t);
+
+		// 中空円筒の慣性モーメント
+		// Iyy = m * ((din^2 + dout^2) / 16 + Lf / 12)
+		double IjFuelPitch = massFuel * ((Math.pow(dFuelOut, 2) + Math.pow(dFuelIn, 2)) / 16.0 + lFuel / 12.0); //fuelの重心回りの慣性モーメント
+		IjFuelPitch += massFuel * Math.pow(lcgFuel - lcg, 2); // parallel axis thorem
+
+		// oxidizer
+		double massOx = getMassOxidizer(t);
+		double lengthOx = lengthTank * t / timeBurnout;
+		/*oxidizerの慣性モーメント
+		 * スロッシングの影響を無視し,液体酸化剤を一様円柱と仮定して計算 **/
+		double IjOxPitch = massOx * (Math.pow(dTank, 2) / 16.0 + Math.pow(lengthOx, 2) / 12.0); //oxidizer自身の重心回りの慣性モーメント
+		IjOxPitch += massOx * Math.pow(getLcgOx(t) - lcg, 2); // parallel axis theorem
+
+		IjPropPitch = IjOxPitch + IjFuelPitch;
+
+		return IjPropPitch;
+	}
 
 	public double Ij_roll(double t) {
 		double Ij_roll;
 
-		if(t < time_Burnout) {
-			Ij_roll = Ij_roll_0 + t*(Ij_roll_f - Ij_roll_0) / time_Burnout;
-		}else {
-			Ij_roll = Ij_roll_f;
+		if (t < timeBurnout) {
+			Ij_roll = IjRollBef + t * (IjRollAft - IjRollBef) / timeBurnout;
+		} else {
+			Ij_roll = IjRollAft;
 		}
 
 		return Ij_roll;
 	}
 
-
 	public double Ij_pitch(double t) {
 		double Ij_pitch;
 
-		if(t < time_Burnout) {
-			Ij_pitch = Ij_pitch_0 + t*(Ij_pitch_f - Ij_pitch_0) / time_Burnout;
-		}else {
-			Ij_pitch = Ij_pitch_f;
+		if (t < timeBurnout) {
+			Ij_pitch = IjPitchBef + t * (IjPitchAft - IjPitchBef) / timeBurnout;
+		} else {
+			Ij_pitch = IjPitchAft;
 		}
 
 		return Ij_pitch;
 	}
 
-
-	public double Lcg(double t){
+	public double getLcg(double t) {
 		double Lcg;
 
-		if(t < time_Burnout) {
-			Lcg = Lcg_0 + (Lcg_f - Lcg_0) * t / time_Burnout;
-		}else {
-			Lcg = Lcg_f;
+		if (t < timeBurnout) {
+			Lcg = lcgBef + (lcgAft - lcgBef) * t / timeBurnout;
+		} else {
+			Lcg = lcgAft;
 		}
 
 		return Lcg;
 	}
 
+	//実装中
+	public double getLcgProp(double t) {
+		if(t < timeBurnout) {
+			return getMassFuel(t) * lcgFuel + getMassOxidizer(t) * getLcgOx(t);
+		}else {
+			return 0.0;
+		}
+	}
+
+	/**
+	 * claculate oxidizer's cnter of gravity
+	 * @param t [sec]
+	 * @return lcgOx 機体先端からoxidizer重心までの距離
+	 * */
+	public double getLcgOx(double t) {
+		double lengthOx = lengthTank * (t / timeBurnout); //oxidizerを円柱としたときの円柱の長さ
+		return l - (lMotor + lengthOx);
+	}
 
 	public double thrust(double t) {
-		double thrust ;
+		double thrust;
 
-		if(t < time_Burnout) {
-			thrust = thrust_analy.linearInterp1column(t);
-		}else {
+		if (t < timeBurnout) {
+			thrust = thrustAnaly.linearInterp1column(t);
+		} else {
 			thrust = 0.0;
 		}
 
