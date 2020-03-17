56fc5991c97514f8af69f9ed4a3562d0fddfa6c0
zoooi <zoooi@133.51.96.36>
2020-03-17 18:08:56 +0900

    inserting Tip off dynamics

--- src/quabla/simulator/dynamics/DynamicsTipOff.java
+++ src/quabla/simulator/dynamics/DynamicsTipOff.java
@@ -29,36 +29,36 @@ public class DynamicsTipOff extends AbstractDynamics {
 		// Import variable
 		double t = variable.getTime();
 		double altitude = variable.getAltitude();
-		double distance_LowerLug = variable.getDistanceLowerLug();
-		double distance_UpperLug = variable.getDistanceUpperLug();
-		MathematicalVector vel_ENU = variable.getVel_ENU();
-		MathematicalVector omega_BODY = variable.getOmega_Body();
+		double distanceLowerLug = variable.getDistanceLowerLug();
+		double distanceUpperLug = variable.getDistanceUpperLug();
+		MathematicalVector velENU = variable.getVel_ENU();
+		MathematicalVector omegaBODY = variable.getOmega_Body();
 		MathematicalVector quat = new MathematicalVector(Coordinate.nomalizeQuat(variable.getQuat().getValue()));
 
 		double m = rocket.getMass(t);
 		double mDot = rocket.mdot(t);
-		double p = omega_BODY.getValue()[0];
-		double q = omega_BODY.getValue()[1];
-		double r = omega_BODY.getValue()[2];
+		double p = omegaBODY.getValue()[0];
+		double q = omegaBODY.getValue()[1];
+		double r = omegaBODY.getValue()[2];
 
 		// Transition coordinate
-		MathematicalMatrix dcm_ENU2BODY = new MathematicalMatrix(Coordinate.getDCM_ENU2BODYfromQuat(quat.getValue()));
-		MathematicalMatrix dcm_BODY2ENU = dcm_ENU2BODY.transpose();
+		MathematicalMatrix dcmENU2BODY = new MathematicalMatrix(Coordinate.getDCM_ENU2BODYfromQuat(quat.getValue()));
+		MathematicalMatrix dcmBODY2ENU = dcmENU2BODY.transpose();
 
-		// wind
-		MathematicalVector wind_ENU = new MathematicalVector(Wind.wind_ENU(wind.getWindSpeed(altitude), wind.getWindDirection(altitude)));
-		MathematicalVector vel_airENU = vel_ENU.sub(wind_ENU);
-		MathematicalVector vel_airBODY = dcm_ENU2BODY.dot(vel_airENU);
-		double vel_airAbs = vel_airBODY.norm();
-		double v = vel_airBODY.getValue()[1];
-		double w = vel_airBODY.getValue()[2];
+		// Wind
+		MathematicalVector windENU = new MathematicalVector(Wind.wind_ENU(wind.getWindSpeed(altitude), wind.getWindDirection(altitude)));
+		MathematicalVector velAirENU = velENU.sub(windENU);
+		MathematicalVector velAirBODY = dcmENU2BODY.dot(velAirENU);
+		double velAirAbs = velAirBODY.norm();
+		double v = velAirBODY.getValue()[1];
+		double w = velAirBODY.getValue()[2];
 		double alpha, beta;
-		if(vel_airAbs <= 0.0) {
+		if(velAirAbs <= 0.0) {
 			alpha = 0.0;
 			beta = 0.0;
 		}else {
-			alpha = Math.asin(w / vel_airAbs);
-			beta = Math.asin(v / vel_airAbs);
+			alpha = Math.asin(w / velAirAbs);
+			beta = Math.asin(v / velAirAbs);
 		}
 
 		// Environment
@@ -67,26 +67,80 @@ public class DynamicsTipOff extends AbstractDynamics {
 		double P = atm.getAtomosphericPressure(altitude);
 		double rho = atm.getAirDensity(altitude);
 		double Cs = atm.getSoundSpeed(altitude);
-		double Mach = vel_airAbs / Cs;
-		double dynamics_pressure = 0.5 * rho * Math.pow(vel_airAbs, 2);
+		double Mach = velAirAbs / Cs;
+		double pressureDynamics = 0.5 * rho * Math.pow(velAirAbs, 2);
 
-		//Thrust
+		// Thrust
 		MathematicalVector thrust ;
 		if(rocket.thrust(t) > 0.0) {
-			double thrust_pressure = (P0 - P)* rocket.Ae;
-			thrust = new MathematicalVector(rocket.thrust(t) + thrust_pressure, 0.0, 0.0);
+			double thrustPressure = (P0 - P)* rocket.Ae;
+			thrust = new MathematicalVector(rocket.thrust(t) + thrustPressure, 0.0, 0.0);
 		}else {
 			thrust = new MathematicalVector(0.0, 0.0, 0.0);
 		}
 
 		// Aero Force
-		double drag = dynamics_pressure * aero.Cd(Mach) * rocket.S;
-		//double nomal = dynamics_pressure * aero.CNa(Mach) * rocket.S * alpha;
-		double side = dynamics_pressure * aero.CNa(Mach) * rocket.S * beta;
-		MathematicalVector f_aero = new MathematicalVector(- drag, 0.0, 0.0);
+		double drag = pressureDynamics * aero.Cd(Mach) * rocket.S;
+		double normal = pressureDynamics * aero.CNa(Mach) * rocket.S * alpha;
+		double side = pressureDynamics * aero.CNa(Mach) * rocket.S * beta;
+		MathematicalVector forceAero = new MathematicalVector(- drag, - side, - normal);
+
+		// Newton Equation
+		MathematicalVector forceENU = dcmBODY2ENU.dot(thrust.add(forceAero));
+		MathematicalVector accENU = (forceENU.multiply(1 / m)).add(g);
+
+		// Center of Gravity , Pressure
+		double lcg = rocket.getLcg(t);
+		double lcgProp = rocket.getLcgProp(t);
+		double lcp = aero.Lcp(Mach);
+
+		double pointPivot = distanceLowerLug;
+
+		// Moment of Inertia
+		double IjPitch = rocket.getIjPitch(t) + m * Math.pow(distanceLowerLug - lcg, 2);
+		double IjRoll = rocket.getIjRoll(t);
+		double[] Ij = {IjRoll, IjPitch, IjPitch};
+
+		double IjDotPitch = rocket.getIjDotPitch(t) + m * Math.pow(distanceLowerLug - lcg, 2);
+		double IjDotRoll = rocket.getIjDotRoll(t);
+		double[] IjDot = {IjDotRoll, IjDotPitch, IjDotPitch};
+
+		// Aero Moment
+		MathematicalVector armMoment = new MathematicalVector(distanceLowerLug - lcp, 0.0, 0.0);
+		MathematicalVector momentAero = armMoment.cross(forceAero);
+
+		// Aero Damping Moment
+		MathematicalVector momentAeroDamping = new MathematicalVector(
+				0.0,
+				0.0,
+				pressureDynamics * aero.Cnr * rocket.S *(0.5*Math.pow(rocket.L, 2)/velAirAbs) * r);
+
+		// Jet Damping Moment
+		MathematicalVector momentJetDamping = new MathematicalVector(
+				0.0,
+				0.0,
+				(-IjDot[2] - mDot * (Math.pow(lcg - lcgProp, 2) - Math.pow(rocket.L - lcgProp, 2))) * r);
+
+		MathematicalVector momentGyro = new MathematicalVector(
+				0.0,
+				0.0,
+				(Ij[0] - Ij[1])*p*q);
+
+		MathematicalVector moment = momentGyro.add(momentAero).add(momentAeroDamping).add(momentJetDamping);
+		MathematicalVector omegadot = new MathematicalVector(
+				0.0,
+				0.0,
+				moment.getValue()[2] / Ij[2]);
+
+		// Kinematics Equation
+		MathematicalMatrix tensor = new MathematicalMatrix(Coordinate.Omega_tensor(p, q, r));
+		MathematicalVector quatdot = tensor.dot(quat).multiply(0.5);
 
 		DynamicsMinuteChangeTrajectory delta = new DynamicsMinuteChangeTrajectory();
-
+		delta.setDeltaPos_ENU(velENU);
+		delta.setDeltaVelENU(accENU);
+		delta.setDeltaOmegaBODY(omegadot);
+		delta.setDeltaQuat(quatdot);
 
 		return delta;
 	}
