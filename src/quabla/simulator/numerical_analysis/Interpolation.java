6608ae452abe60219a90eb5da2aa2870174d21c5
zoooi <zoooi@192.168.0.129>
2020-03-20 21:34:53 +0900

    refactoring

--- src/quabla/simulator/numerical_analysis/Interpolation.java
+++ src/quabla/simulator/numerical_analysis/Interpolation.java
@@ -1,77 +1,83 @@
 package quabla.simulator.numerical_analysis;
 
+/**
+ * Interpolation is a class for function interpolation.
+ * */
 public class Interpolation {
-	double x_array[];
-	double y_array[][];
+	double xArray[];
+	double yArray[][];
 	private int length;
 	private int col; //column
 
-	public Interpolation(double[] x_array, double[] y_array){
-		length = x_array.length;
-		this.x_array = new double[length];
+	public Interpolation(double[] xArray, double[] yArray) {
+		length = xArray.length;
+		this.xArray = new double[length];
 		// Deep Copy
-		System.arraycopy(x_array, 0, this.x_array, 0, length);
+		System.arraycopy(xArray, 0, this.xArray, 0, length);
 
-		this.y_array = new double[length][1];
-		for(int i=0; i<length; i++) {
-			this.y_array[i][0] = y_array[i];
+		this.yArray = new double[length][1];
+		for (int i = 0; i < length; i++) {
+			this.yArray[i][0] = yArray[i];
 		}
 	}
 
-	public Interpolation(double[] x_array, double[][] y_array) {
-		length = x_array.length;
-		this.x_array = new double[length];
-		System.arraycopy(x_array, 0, this.x_array, 0, length);
+	public Interpolation(double[] xArray, double[][] yArray) {
+		length = xArray.length;
+		this.xArray = new double[length];
+		System.arraycopy(xArray, 0, this.xArray, 0, length);
 
-		col = y_array[0].length;
-		this.y_array = new double[length][col];
-		for(int i=0; i<length; i++) {
-			System.arraycopy(y_array[i], 0, this.y_array[i], 0, col);
+		col = yArray[0].length;
+		this.yArray = new double[length][col];
+		for (int i = 0; i < length; i++) {
+			System.arraycopy(yArray[i], 0, this.yArray[i], 0, col);
 		}
 	}
 
+	/** 1次元の線形補間 */
 	public double linearInterp1column(double x) {
 		double y;
 		int count = 0;
 
 		//xが取得データの範囲外の場合,最大値及び最小値をyの値とする
-		if(x <= x_array[0]) {
-			y = y_array[0][0];
-		}else if(x >= x_array[length-1]) {
-			y = y_array[length-1][0];
-		}else {
+		if (x <= xArray[0]) {
+			y = yArray[0][0];
+		} else if (x >= xArray[length - 1]) {
+			y = yArray[length - 1][0];
+		} else {
 			//xが何群目に所属しているかを調べる
-			for(int j=0; j<length; j++) {
-				if(x >= x_array[j]) {
+			for (int j = 0; j < length; j++) {
+				if (x >= xArray[j]) {
 					count = j;
-				}else {
+				} else {
 					break;
 				}
 			}
-			y = y_array[count][0] + (y_array[count+1][0] - y_array[count][0])*(x - x_array[count])/(x_array[count+1] - x_array[count]);
+			y = yArray[count][0] + (yArray[count + 1][0] - yArray[count][0]) * (x - xArray[count])
+					/ (xArray[count + 1] - xArray[count]);
 		}
 		return y;
 	}
 
+	/** 他次元の線形補間 */
 	public double[] linearInterpPluralColumns(double x) {
 		double[] y = new double[col];
 		int count = 0;
 
-		for(int i=0; i<col; i++) {
-			if(x <= x_array[0]) {
-				y[i] = y_array[0][i];
-			}else if(x >= x_array[length - 1]) {
-				y[i] = y_array[length - 1][i];
-			}else {
-				for(int j=0; j<length; j++) {
-					if(x >= x_array[j]) {
+		for (int i = 0; i < col; i++) {
+			if (x <= xArray[0]) {
+				y[i] = yArray[0][i];
+			} else if (x >= xArray[length - 1]) {
+				y[i] = yArray[length - 1][i];
+			} else {
+				for (int j = 0; j < length; j++) {
+					if (x >= xArray[j]) {
 						count = j;
-					}else {
+					} else {
 						break;
 					}
 				}
-				y[i] = y_array[count][i] + (y_array[count + 1][i] - y_array[count][i])
-						*(x - x_array[count])/(x_array[count + 1] - x_array[count]);
+				y[i] = yArray[count][i] + (yArray[count + 1][i] - yArray[count][i])
+						* (x - xArray[count]) / (xArray[count + 1] - xArray[count]);
 			}
 		}
 		return y;
